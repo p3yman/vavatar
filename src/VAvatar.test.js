1fb5087b032a4983003f3e9dbb7a309d59fce7f5
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-07-26 19:43:38 +0300

    Initial commit

---
+++ src/VAvatar.test.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils';
+import VAvatar from './VAvatar.vue';
+
+describe('VAvatar.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(VAvatar, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch('VAvatar');
+  });
+});
---
+++ src/VAvatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="vavatar">
+    VA
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VAvatar',
+};
+</script>
+
+<style>
+.vavatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: rosybrown;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
+    'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  font-weight: bold;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+}
+</style>
